7a518120aacd9db2ab8af1c9885a0fcae3597045
Matias L. Soria S. <matuleosoria221@gmail.com>
2025-08-23 22:37:14 -0300

    Cambios: Se agregó validación en el ingreso de datos, aun sin agregar guardado de datos en archivo.

--- Matias_Soria_Examen.cpp
+++ Matias_Soria_Examen.cpp
@@ -1,25 +1,56 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
+#define max_titulo 100
+#define max_autor 50	
 using namespace std;
 //Declaracion de la estructura "libro"
 struct libros{
-	char titulo[100];
-	char autor[50];
+	char titulo[max_titulo];
+	char autor[max_autor];
 	int publicacion;
 }libro[100];
 
 ///FUNCIONES///
 //Funcion ingreso de libros
-int ingreso(int cl) {
+void ingreso(int* cl) {
 	printf("\nIngreso de nuevo libro:\n");
-	printf("Ingrese el titulo del libro:(Sin espacios) ");
-	scanf("%s",libro[cl].titulo);
-	printf("Ingrese el Autor de libro:(Sin espacios) ");
-	scanf("%s",libro[cl].autor);
-	printf("Ingrese el a�o de publicacion del libro: ");
-	scanf("%d",&libro[cl].publicacion);
-	return cl++;
+	printf("Ingrese el titulo del libro:");
+	fgets(libro[*cl].titulo,max_titulo, stdin);
+    //Validación de datos de titulo ingresado
+	size_t len = strlen(libro[*cl].titulo);
+    if (len > 0 && libro[*cl].titulo[len-1] == '\n') {
+        libro[*cl].titulo[len-1] = '\0';
+    }
+    if (strlen(libro[*cl].titulo) == 0) {
+        printf("Error: La etiqueta no puede estar vacía.\n");
+        return;
+    }
+
+	printf("Ingrese el autor del libro:");
+	fgets(libro[*cl].autor,max_autor, stdin);
+    //Validación de datos de autor ingresado
+	size_t len = strlen(libro[*cl].autor);
+    if (len > 0 && libro[*cl].autor[len-1] == '\n') {
+        libro[*cl].autor[len-1] = '\0';
+    }
+    if (strlen(libro[*cl].autor) == 0) {
+        printf("Error: La etiqueta no puede estar vacía.\n");
+        return;
+    }
+
+	printf("Ingrese el año de publicacion del libro: ");
+ 	if (scanf("%d", &libro[*cl].publicacion) != 1) {
+        printf("Error: Debe ingresar un número válido.\n");
+        while (getchar() != '\n');
+        return;
+    }
+	if (libro[*cl].publicacion < 0) {
+        printf("Error: El año no puede ser negativo.\n");
+        while (getchar() != '\n');
+        return;
+    }
+	*cl++;
 }
 //funcion para mostrar los libros cargados
 void mostrar(int cl){
@@ -87,42 +118,70 @@ void bano(int a, int cl){
 //funcion para buscar un libro 
 void buscar(int cl){
 	int opc,a;
-	char tit[100],aut[50]; 
+	char tit[max_titulo],aut[max_autor]; 
 	printf("Como desea buscar el archivo.\n");
 	printf("1- Por titulo.");
 	printf("2- Por autor.");
-	printf("3- Por a�o de publicaci�n.");
+	printf("3- Por año de publicación.");
 	switch(opc){
 	case 1:
 		printf("Ingrese el titulo del libro:\n");
-		scanf("%s",tit);
+		fgets(tit,max_titulo, stdin);
+    	//Validación de datos de titulo ingresado
+		size_t len = strlen(tit);
+    	if (len > 0 && tit[len-1] == '\n') {
+        	tit[len-1] = '\0';
+    	}
+    	if (strlen(tit) == 0) {
+        	printf("Error: La etiqueta no puede estar vacía.\n");
+        return;
+    	}
 		btitulo(tit, cl);
 		break;
 	case 2:
 		printf("Ingrese el autor del libro:\n");
-		scanf("%s",aut);
+		fgets(aut,max_autor, stdin);
+    	//Validación de datos de autor ingresado
+		size_t len = strlen(aut);
+    	if (len > 0 && aut[len-1] == '\n') {
+        	aut[len-1] = '\0';
+    	}
+    	if (strlen(aut) == 0) {
+        	printf("Error: La etiqueta no puede estar vacía.\n");
+        return;
+    	}
 		bautor(aut, cl);
 		break;
 	case 3:
 		printf("Ingrese el a�o de publicaci�n del libro:\n");
-		scanf("%d",&a);
+		printf("Ingrese el año de publicacion del libro: ");
+ 		if (scanf("%d", &a) != 1) {
+        	printf("Error: Debe ingresar un número válido.\n");
+        	while (getchar() != '\n');
+        	return;
+    	}
+		if (a < 0) {
+        	printf("Error: El año no puede ser negativo.\n");
+       		while (getchar() != '\n');
+        	return;
+    	}
 		bano(a,cl);
 	}
 }
 //funcion para guardar archivo
-void guardara (int cl){
+/*void guardara (int cl){
 	FILE* lib;
 	lib=fopen("libros.txt","w");
 	if (lib == NULL){
 		printf("Error al abrir el archivo.");
 	}
 	fprintf("\n\n");
-	fprintf("Libro N�: %d\n", i);
+	fprintf("Libro N°: %d\n", i);
 	fprintf("Titulo: %s\n",libro[i].titulo);
 	fprintf("Autor: %s\n",libro[i].autor);
-	fprintf("A�o de publicacion: %d\n",libro[i].publicacion);
+	fprintf("Año de publicacion: %d\n",libro[i].publicacion);
 }
-
+*/
 //FUNCION PRINCIPAL
 int main(int argc, char *argv[]) {
 	int op;
@@ -137,7 +196,7 @@ int main(int argc, char *argv[]) {
 		scanf("%d",&op);
 		switch(op){
 		case 1:
-			cl = ingreso(cl);//Por cada libro que se ingresa cl (Cantida libros) aumenta 
+			ingreso(&cl);//Por cada libro que se ingresa cl (Cantida libros) aumenta 
 			break;
 		case 2:
 			mostrar(cl);//Muestra los libros almacenados
@@ -146,7 +205,7 @@ int main(int argc, char *argv[]) {
 			buscar(cl);
 			break;
 		case 4:
-			guardara(cl);
+			//guardara(cl);
 			break;
 		case 5:
 			printf("Saliendo....\n");
